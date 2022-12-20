846e3f6e82251de9e7ef5f7da24bb5778af5826c
supto51 <suptokhan25@gmail.com>
2022-12-21 00:59:33 +0600

    urls & template added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+
+def posts(request):
+    return render(request, 'blog/posts.html')
+
+
+def post_details(request, slug):
+    return render(request, 'blog/post-details.html')
