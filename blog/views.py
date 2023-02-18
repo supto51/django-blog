f85c58bb6f2074fb5440269e9f2eb8ec627cfdef
Supto <supto51>
2023-02-18 17:54:41 +0600

    new models created

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ all_posts = [
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
         "author": "Supto",
-        "date": date(2021, 7, 21),
+        "date": date(2023, 7, 21),
         "title": "Mountain Hiking",
         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
         "content": """
@@ -26,7 +26,7 @@ all_posts = [
         "slug": "programming-is-fun",
         "image": "coding.jpg",
         "author": "Supto",
-        "date": date(2022, 3, 10),
+        "date": date(2023, 3, 10),
         "title": "Programming Is Great!",
         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
         "content": """
